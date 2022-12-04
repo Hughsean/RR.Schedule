0d86d0270c9aa8944599e6badfeec5e4f1d0c9a2
xSeung <xSeung@163.com>
2022-12-04 23:22:09 +0800

    初始化

---
+++ inc/library.h
@@ -0,0 +1,13 @@
+#ifndef A_C_LIB_LIBRARY_H
+#define A_C_LIB_LIBRARY_H
+
+
+struct pcb {
+    char   name[32];
+    double arrivetime;
+    double starttime;
+    double servicetime;
+};
+void test();
+
+#endif  // A_C_LIB_LIBRARY_H
---
+++ test/test.cpp
@@ -0,0 +1,11 @@
+extern "C" {
+#include "library.h"
+}
+
+#include <iostream>
+#include <format>
+int main() {
+    test();
+    std::cout<<std::format("wqiodh\n");
+    return 0;
+}
